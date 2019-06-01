3d287653b11aedc632906e402fa7f983c4113029
Sachin Gadgil <44280456+sachingadgil@users.noreply.github.com>
2019-06-01 19:21:51 -0400

    added gui

---
+++ Mouse as a Paint-Brush.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy as np
+import random
+import tkinter as tk
+from tkinter import simpledialog
+from datetime import datetime
+import os
+os.chdir('C:\\Users\\sachi\\.vscode\\GitHubRepos\\OSCV_Exercises')
+exetasknum=1
+
+def imgnamedialogue():
+    application_window = tk.Tk()
+    application_window.lift()
+    answer = simpledialog.askstring("Input", "What is your first name?", parent=application_window)
+    return answer
+
+if exetasknum==1:
+    # mouse callback function
+    #original function
+    def draw_circle(event,x,y,flags,param):
+        if event == cv2.EVENT_LBUTTONDBLCLK:
+            cv2.circle(img,(x,y),100,(255,100,255),-1)
+    #myfunction with randomization
+    def draw_randcircle(event,x,y,flags,param):
+        if event == cv2.EVENT_LBUTTONUP:
+            cv2.circle(img,(x,y), random.randint(30,50),(random.randint(0,255),random.randint(0,255),random.randint(0,255)),-1)
+    # Create a black image, a window and bind the function to window
+    img = np.zeros((900,1600,3), np.uint8)
+    img.fill(255) # to make the image backgroud white
+    cv2.namedWindow('image')
+    cv2.setMouseCallback('image',draw_randcircle)
+
+    while(1):
+        cv2.imshow('image',img)
+        if cv2.waitKey(20) & 0xFF == 27:
+            #filename=input("enter image file name")
+            filename = imgnamedialogue()
+            stamp=str(datetime.now())
+            filenamewithpath=os.getcwd()+"\\"+filename+".jpg"
+            print(filenamewithpath)
+            cv2.imwrite(filenamewithpath,img)
+            break
+    cv2.destroyAllWindows()
+
