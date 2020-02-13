69db53ea42f965dedffd265e2216e22e0527e879
Sachin Gadgil <44280456+sachingadgil@users.noreply.github.com>
2020-02-13 00:42:05 -0500

    Adding exercises from the course

---
+++ Master_Computer_Vision_RDR/Original/Beginning Tips.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #### See where your python.exe is located 
+
+# In[ ]:
+
+
+import sys
+print(sys.executable)
+
+
+# #### See your working directory
+
+# In[ ]:
+
+
+import os
+print(os.getcwd())
+
+
+# #### Test your imports
+
+# In[ ]:
+
+
+import cv2
+import numpy as np
+import matplotlib
+
+
+# #### Display the version of OpenCV 
+
+# In[ ]:
+
+
+print cv2.__version__
+
